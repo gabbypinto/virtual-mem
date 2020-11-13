bd4321e514b498986aef14a385d9dc89b61d4b35
gabbypinto <gabrielapintogp@gmail.com>
2020-11-13 12:31:44 -0800

    file read done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # virtual-mem
+
+#### references
+- https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/fread?view=msvc-160
+- http://www2.cs.uregina.ca/~hamilton/courses/330/notes/memory/paging.html
+
+#### other
+- https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
--- vmmgr.c
+++ vmmgr.c
@@ -6,31 +6,48 @@
 #define TLB_ENTRIES 16
 //global page size variable
 
-struct{
+typedef struct{
   int tag;
   int physicalPageNumber;
-} tblObject;
+} tlbObject;
 
 
 int main(int argc, char *argv[]){
+  tlbObject *tlb;
+  tlb = malloc(TLB_ENTRIES);
+
+  FILE *fp;
+  int numAddresses;
+  char buffer[100000]; //contents in addresses.txt
 
   if (argc != 2) {
        printf("Usage: %s <addresses.txt> \n", argv[0]);
        return -1;
    }
 
+  char* fileName = argv[1];
+  /* Open file for both reading */
+  int count=0;
+  fp = fopen(fileName, "r");
+  if (fp == NULL){
+    fprintf((stderr), "File didn't open :( \n" );
+    exit(1);
+  }
+  numAddresses = fread(buffer,sizeof(char),sizeof(buffer),fp);
+  printf("number of items read=  %d\n", numAddresses);
+  //printf( "Contents of buffer = %.100000s\n", buffer );
+  printf("%s",buffer);
+  fclose(fp);
+
+  //convert int to binary for offset calc
 
    //read addresses .txt, use fgets()
     //translate each address and translate it to its corresponding physical addresses
 
-
-
    //read binary file/backing_store.bin binary file
       //use fopen(),fread(),fseek(), and fclose()
 
-
-    tblObject tlb = tblObject[TLB_ENTRIES];
-
+  return (0);
    //output the value of the signed byte at the physical address
 
 }
