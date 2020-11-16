d847587f765638e35d8a0d5c813dc3d6f5b124d9
gabbypinto <gabrielapintogp@gmail.com>
2020-11-15 23:37:03 -0800

    logical pg,offset

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 #### references
 - https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/fread?view=msvc-160
 - http://www2.cs.uregina.ca/~hamilton/courses/330/notes/memory/paging.html
+- https://cs.stackexchange.com/questions/7743/how-to-work-out-physical-address-corresponding-to-logical-address
 
 #### other
 - https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
--- vmmgr.c
+++ vmmgr.c
@@ -4,12 +4,26 @@
 #include <stdbool.h>
 
 #define TLB_ENTRIES 16
-//global page size variable
+#define PAGE_SIZE 256  //page size of 256 bytes
+#define PAGES 256  //pages begin at 0-255
+#define MEMORY_SIZE PAGES * PAGE_SIZE  //bin file size of 65,536 bytes
+#define OFFSET 8   //offset bits
+
+#define PAGE_MASK 255  //see page table
+#define OFFSET_MASK 255 //0-255 see physical mem table
+
+
 
 typedef struct{
-  int tag;
-  int physicalPageNumber;
+  //we're ints before
+  unsigned char tag; //logical address
+  unsigned char  physicalPageNumber; //physical address
 } tlbObject;
+
+int searchTlbTable(unsigned char logPage){
+  int i;
+  return -1;
+}
 
 
 int main(int argc, char *argv[]){
@@ -17,33 +31,84 @@ int main(int argc, char *argv[]){
   tlb = malloc(TLB_ENTRIES);
 
   FILE *fp;
-  int numAddresses;
+  int numAddresses = 0; //total num addresses
   char buffer[6830]; //contents in addresses.txt
+
+  int logicalAddresses; //the int value being read from addresses.txt
+
+  int pageFaults; //page fault rate = % of address references -> page faults
+  int tlbHits; //TLB hit rate = % of addreses references that were resolved in the TLB
+
 
   if (argc != 2) {
        printf("Usage: %s <addresses.txt> \n", argv[0]);
        return -1;
    }
 
+   //read addresses.txt
   char* fileName = argv[1];
-  /* Open file for both reading */
-  int count=0;
   fp = fopen(fileName, "r");
   if (fp == NULL){
     fprintf((stderr), "File didn't open :( \n" );
     exit(1);
   }
-  numAddresses = fread(buffer,sizeof(char),sizeof(buffer),fp);
+
+
+  // numAddresses = fread(buffer,sizeof(char),sizeof(buffer),fp);
   //sprintf("number of items read=  %d\n", numAddresses);  6831
 
   //printf( "Contents of buffer = %.100000s\n", buffer );
-  //printf("%s",buffer);
+  // printf("%c\n",buffer[0]);
 
-  for(int i=0;i<sizeof(buffer);i++){
-    printf("%c",buffer[i]);
+//prints the buffer/addresses
+  int count = 0;
+  // for(int i=0;i<sizeof(buffer);i++){
+  //   printf("buffer: %c\n",buffer[i]);
+  //   printf("buffer atoi:%d \n", atoi(&buffer[i]));
+  //   printf("buffer elem size: %lu \n",sizeof(atoi(&buffer[i])));
+  //   if(buffer[i]=='\n'){
+  //     printf("hi\n");
+  //   }
+  //
+  //   // numAddresses++;
+  //   // int logicalAddress = atoi(&buffer[i]);
+  //   // printf("logical address: %d\n",logicalAddress);
+  //   // int offset = logicalAddress & OFFSET_MASK;
+  //   // printf("offset: %d\n",offset);
+  //   // int logicalPage = (logicalAddress >> OFFSET) & PAGE_MASK;
+  //   // printf("logical page: %d\n", logicalPage);
+  //   if(count==10){
+  //     break;
+  //   }
+  //   count++;
+  // //
+  // }
+
+  while(fgets(buffer,6830,fp)!=NULL){
+    printf("%s",buffer);
+    numAddresses++;
+    int logicalAddress = atoi(buffer);
+    printf("logical address: %d\n",logicalAddress);
+    int offset = logicalAddress % 256;
+    printf("offset: %d\n",offset);
+    int logicalPage = logicalAddress/256;
+    printf("logical page: %d\n", logicalPage);
+    int physicalPage; //search in tbh
+    printf("\n ");
+    if(count==5){
+      break;
+    }
+    count++;
   }
 
-  fclose(fp);
+  fclose(fp);     //close file
+
+
+
+
+
+
+
 
   //convert int to binary for offset calc
 
